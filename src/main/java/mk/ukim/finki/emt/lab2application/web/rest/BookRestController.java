74c662d88c15427d7e527e1133fbfbfc782b47ac
ivonakitanovska <ivonakitanovska85@gmail.com>
2022-04-25 21:48:23 +0200

    Change CORS

--- src/main/java/mk/ukim/finki/emt/lab2application/web/rest/BookRestController.java
+++ src/main/java/mk/ukim/finki/emt/lab2application/web/rest/BookRestController.java
@@ -17,7 +17,7 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*")
 @RequestMapping("/api")
 public class BookRestController {
     private final BookService bookService;
